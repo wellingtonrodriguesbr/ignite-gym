035498c8a36f32621adeb215c3a01ada32da4733
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-30 06:15:39 -0300

    style: update background and color

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,8 +2,8 @@ import { Spinner, Center } from "native-base";
 
 export function Loading() {
   return (
-    <Center flex={1} bg="pink.400">
-      <Spinner />
+    <Center flex={1} bg="gray.700">
+      <Spinner color="green.500" />
     </Center>
   );
 }
