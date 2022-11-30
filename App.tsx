df3c4c6260ab42a1755be817600c3c3fa6ef1123
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-11-30 06:13:29 -0300

    chore: add native base and create a custom theme

--- App.tsx
+++ App.tsx
@@ -3,7 +3,10 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
-import { Text, View, StatusBar } from "react-native";
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { Loading } from "@components/Loading";
+import { theme } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,13 +15,13 @@ export default function App() {
   });
 
   return (
-    <View>
+    <NativeBaseProvider theme={theme}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Text>Hello World!</Text>
-    </View>
+      <Loading />
+    </NativeBaseProvider>
   );
 }
