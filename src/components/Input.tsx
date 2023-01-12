0c2693cf58fb939baa2f63df0f2779cc7377ff21
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-12 06:50:40 -0300

    feat: create input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { IInputProps, Input as NativeBaseInput } from "native-base";
+
+export function Input({ ...rest }: IInputProps) {
+  return (
+    <NativeBaseInput
+      backgroundColor="gray.700"
+      borderWidth={0}
+      fontSize="md"
+      color="white"
+      fontFamily="body"
+      mb={4}
+      h={14}
+      px={4}
+      placeholderTextColor="gray.300"
+      _focus={{
+        backgroundColor: "gray.700",
+        borderWidth: 1,
+        borderColor: "green.500",
+      }}
+      {...rest}
+    />
+  );
+}
