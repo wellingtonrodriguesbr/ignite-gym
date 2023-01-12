fda3907d56a36297157555dbb9882f8d8105aad2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-12 06:50:52 -0300

    feat: WIP Sign in screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,15 +1,39 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Uma mulher e um homem pedalando em uma bicicleta ergométrica na academia"
         resizeMode="contain"
         position="absolute"
       />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+
+      <Heading
+        color="gray.100"
+        mx="auto"
+        mb={6}
+        fontSize="xl"
+        fontFamily="heading"
+      >
+        Acesse sua conta
+      </Heading>
+      <Input
+        keyboardType="email-address"
+        autoCapitalize="none"
+        placeholder="E-mail"
+      />
+      <Input secureTextEntry placeholder="Senha" />
     </VStack>
   );
 }
