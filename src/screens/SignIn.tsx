9a502cc36af580d1b09551a34c13fba79ca8eb53
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-13 06:46:56 -0300

    feat: finished Sign in screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,39 +1,53 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      <Image
-        source={BackgroundImg}
-        alt="Uma mulher e um homem pedalando em uma bicicleta ergométrica na academia"
-        resizeMode="contain"
-        position="absolute"
-      />
-      <Center my={24}>
-        <LogoSvg />
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="Uma mulher e um homem pedalando em uma bicicleta ergométrica na academia"
+          resizeMode="contain"
+          position="absolute"
+        />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
 
-      <Heading
-        color="gray.100"
-        mx="auto"
-        mb={6}
-        fontSize="xl"
-        fontFamily="heading"
-      >
-        Acesse sua conta
-      </Heading>
-      <Input
-        keyboardType="email-address"
-        autoCapitalize="none"
-        placeholder="E-mail"
-      />
-      <Input secureTextEntry placeholder="Senha" />
-    </VStack>
+        <Heading
+          color="gray.100"
+          mx="auto"
+          mb={6}
+          fontSize="xl"
+          fontFamily="heading"
+        >
+          Acesse sua conta
+        </Heading>
+        <Input
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholder="E-mail"
+        />
+        <Input secureTextEntry placeholder="Senha" />
+        <Button text="Acessar" />
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+          <Button text="Criar conta" variant="outline" />
+        </Center>
+      </VStack>
+    </ScrollView>
   );
 }
