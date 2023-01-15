503fc93f117bbc0ae96719ffacd51aa19c7621d5
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-15 09:20:53 -0300

    feat: create HomeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { TouchableOpacity } from "react-native";
+import { Heading, VStack, Text, HStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserPhoto } from "./UserPhoto";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        size={16}
+        source={{ uri: "https://github.com/wellingtonrodriguesbr.png" }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Wellington
+        </Heading>
+      </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
