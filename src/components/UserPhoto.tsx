228b5466f05255970b8b375a08f67726059170a2
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-01-15 09:21:04 -0300

    feat: create UserPhoto component

---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,18 @@
+import { IImageProps, Image } from "native-base";
+
+type UserPhotoProps = IImageProps & {
+  size: number;
+};
+
+export function UserPhoto({ size, ...rest }: UserPhotoProps) {
+  return (
+    <Image
+      w={size}
+      h={size}
+      rounded="full"
+      borderWidth={2}
+      borderColor="gray.400"
+      {...rest}
+    />
+  );
+}
